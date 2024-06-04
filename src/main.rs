90a834638c2fdd42ab8ad11e82b0570ff84b2b6b
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-06-04 23:08:40 +0200

    Receive weather data JSON from OpenWeather

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use colored::Colorize;
+use dotenv::dotenv;
 use serde::Deserialize;
 use std::collections::HashMap;
+use serde_json::Value;
 
+
+// This one's small enough to be worth a declaration
 #[derive(Deserialize, Debug)]
 struct IpData {
     status: String,
@@ -10,16 +14,33 @@ struct IpData {
     city: String,
     lat: f32,
     lon: f32,
-    isp: String
+    isp: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let location_data = reqwest::get("http://ip-api.com/json/?fields=status,message,country,city,lat,lon,isp")
-        .await?
-        .json::<IpData>()
-        .await?;
+    dotenv().ok();
+
+    let location_data =
+        reqwest::get("http://ip-api.com/json/?fields=status,message,country,city,lat,lon,isp")
+            .await?
+            .json::<IpData>()
+            .await?;
     println!("{:?}", location_data);
+
+    let openweather_api_token =
+        std::env::var("OPENWEATHER_KEY").expect("OPENWEATHER_KEY must be set.");
+
+    let weather_data: Value = serde_json::from_str(
+        &reqwest::get(format!(
+            "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}",
+            location_data.lat, location_data.lon, openweather_api_token
+        ))
+        .await?
+        .text()
+        .await?,
+    )?;
+    println!("{:#?}", weather_data);
 
     Ok(())
 }
